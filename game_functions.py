b8bd5f67fdc5945c38f2d03d0b6e02f5aa0bcdc3
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-02-02 16:33:58 +0100

    added q button as means to quit the game

--- game_functions.py
+++ game_functions.py
@@ -15,6 +15,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         # fire bullets
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
         
 
 def check_keyup_events(event, ship):

ce25682de9f24b0ea0198a00e47f6350f94ee44e
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-02-02 16:02:14 +0100

    set up bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -15,6 +16,9 @@ def run_game():
     # Make a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Set the background color.
     bg_color = (230, 230, 230)
 
@@ -22,12 +26,14 @@ def run_game():
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
 
         # Update ship movement
         ship.update()
 
+        bullets.update()
+
         # Update screen
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()

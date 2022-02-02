8c58c15f7041444ebc25785e2cf24beb40b9d8c7
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-02-02 16:06:22 +0100

    get rid of bullets that have dissapeared

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,11 @@ def run_game():
 
         bullets.update()
 
+        #Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
         # Update screen
         gf.update_screen(ai_settings, screen, ship, bullets)
 

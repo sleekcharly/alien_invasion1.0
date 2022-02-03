1585b5bcf66715b317d04d3366278f6ec06a39d0
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-02-03 15:25:53 +0100

    hide the mouse pointer while game is active

--- game_functions.py
+++ game_functions.py
@@ -47,6 +47,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
         # Reset the game statistics
         stats.reset_stats()
         stats.game_active = True
